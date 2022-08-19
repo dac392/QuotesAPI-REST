b333542029077abd9104ae3db8497edf994b44fb
dac392 <dac392@scarletmail.rutgers.edu>
2022-08-19 12:29:44 -0400

    GET quotes and POST quotes

--- app.js
+++ app.js
@@ -1,21 +1,58 @@
 const express = require('express');
-const {records} = require('./data');
+const records = require('./records');
+
 const app = express();
-
-app.get('/quotes', (req, res)=>{
-    res.json(records);
-});
-
-app.get('/quotes/:id', (req, res)=>{
-    // using == bc :id is a strign an quote.id is a number
-    const quote = records.find(quote => quote.id == req.params.id)
-    res.json(quote)
-});
-
+app.use(express.json)
 
 // send a GET request to /quotes to READ a list of quots
+app.get('/quotes', async (req, res)=>{
+    try{
+        const quotes = await records.getQuotes()
+        res.status(201).json(quote)
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+
+});
+
 // send a GET request to /quotes/:id to READ (view) a quote
+app.get('/quotes/:id', async (req, res)=>{
+    // // using == bc :id is a strign an quote.id is a number
+    // const quote = records.find(quote => quote.id == req.params.id)
+    try{
+        const quote = await records.getQuote(req.params.id)
+        if(quote){
+            res.status(201).json(quote)
+        } else{
+            res.status(400).json({message: "Quote was not found"})
+        }
+        
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+
+});
+
+
 // send a POST request to /quotes to CREATE a new quote
+app.post('/quotes', async (req, res)=>{
+    try{
+        if(req.body.author && req.body.quote){
+            const quote = await records.createQuote({
+                quote: req.body.quote,
+                author: req.body.author
+            });
+            res.status(201).json(quote)
+        }else{
+            res.status(404).json({message: "Quote and Author are required"})
+        }
+
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+
+})
+
 // send a PUT request to /quotes/:id to UPDATE (edit) a quote
 // send a DELETE request to /quotes:id DELETE a quote
 // send a GET request to /quotes/quote/random to READ a random quote
